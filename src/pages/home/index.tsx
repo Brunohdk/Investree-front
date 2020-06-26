a1bf6b72b05b1a703b14fe9dd2bc75e54f53a32c
Bruno Hideki <bruno.hideki@t2software.com.br>
2020-06-25 21:53:13 -0300

    structuring home layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
+import { useTheme } from '../../store'
 import './styles.scss'
 
 export default function Home() {
+
+    const { theme, setTheme } = useTheme()
+    
     return (
-        <div className="home-page">
-            
+        <div id="home">
+            <button
+                onClick={() => setTheme('abc')}
+            >
+                {theme}
+            </button>
         </div>
     )
 }
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
+import Button from '../../components/common/Button'
 import './styles.scss'
 
 export default function Landing() {
@@ -20,15 +22,33 @@ export default function Landing() {
                     </header>
                     <main>
                         <div className="apresentation-text">
-                            <h1>
+                            <h1 className="title">
                                 Financial control in a easy way
                             </h1>
-                            <p>
+                            <p className="description">
                                 Total and real-time control of all financial assets in your portfolio.
                             </p>
-                            <Link to="/home">
-                            
+                            <Link
+                                to='/home'
+                                className='btn-link'
+                            >
+                                <span>
+                                    <FiLogIn />
+                                </span>
+                                <p>
+                                    Let's start
+                                </p>
                             </Link>
+                            <img 
+                                className="icon-money"
+                                src={require('../../assets/images/icons/money-bag.svg')} 
+                                alt="$"
+                            />
+                            {/* <img 
+                                className="icon-money"
+                                src={require('../../assets/images/icons/money-circle.svg')} 
+                                alt="$"
+                            /> */}
                         </div>
                         <div className="apresentation-image">
                             <img 
@@ -36,16 +56,6 @@ export default function Landing() {
                                 src={require('../../assets/images/home/landing/landingPage.svg')} 
                                 alt="Logo" 
                             />
-                            {/* <img 
-                                className="icon-money"
-                                src={require('../../assets/images/icons/money-bag.svg')} 
-                                alt="$"
-                            />
-                            <img 
-                                className="icon-money"
-                                src={require('../../assets/images/icons/money-circle.svg')} 
-                                alt="$"
-                            /> */}
                         </div>
                     </main>
                 </div>
